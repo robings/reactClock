82f2b820a7374904203414a13182e3f0fbb68229
robings <58939401+robings@users.noreply.github.com>
2020-06-07 00:06:42 +0100

    changed clock colors

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from "react-dom";
 import FaceTurnClock from "./Components/FaceTurnClock/FaceTurnClock";
 
 const clockSettings = {
-  size: 600,
-  clockOuterColor: "green",
-  clockOuterBorder: "orange",
-  clockInnerColor: "whitesmoke",
-  timePointerColor: "gold",
-  faceNosColor: "gold",
+  size: 300,
+  clockOuterColor: "#0078D7",
+  clockOuterBorder: "#DDDDDD",
+  clockInnerColor: "#FFFFFF",
+  timePointerColor: "0078D7",
+  faceNosColor: "#DDDDDD",
   digitalOn: true,
-  digitalColor: "#EBBD3F",
+  digitalColor: "#DDDDDD",
 };
+
+// const clockSettings = {
+//   size: 600,
+//   clockOuterColor: "green",
+//   clockOuterBorder: "orange",
+//   clockInnerColor: "whitesmoke",
+//   timePointerColor: "gold",
+//   faceNosColor: "gold",
+//   digitalOn: true,
+//   digitalColor: "#EBBD3F",
+// };
 
 ReactDOM.render(
   <FaceTurnClock clockSettings={clockSettings} />,
