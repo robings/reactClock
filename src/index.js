862f0ec33689774f6568a876fc6692b8c80268f5
robings <58939401+robings@users.noreply.github.com>
2020-06-06 23:55:13 +0100

    removed old digital display div

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const clockSettings = {
   clockInnerColor: "whitesmoke",
   timePointerColor: "gold",
   faceNosColor: "gold",
-  digitalOn: false,
+  digitalOn: true,
   digitalColor: "#EBBD3F",
 };
 
