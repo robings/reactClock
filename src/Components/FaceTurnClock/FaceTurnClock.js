e89bf8a35ffc89a97949a61c18434b0394176fa5
robings <58939401+robings@users.noreply.github.com>
2020-06-06 15:38:47 +0100

    added clock face dividers, and started adding day of week display dial

--- src/Components/FaceTurnClock/FaceTurnClock.js
+++ src/Components/FaceTurnClock/FaceTurnClock.js
@@ -2,56 +2,117 @@ import React, { useState, useEffect } from "react";
 import "./faceTurnClock.css";
 
 function FaceTurnClock(props) {
-  const [size] = useState(props.size !== undefined ? props.size : 200);
+  const [size] = useState(
+    props.clockSettings.size !== undefined ? props.clockSettings.size : 200
+  );
   const [clockOuterColor] = useState(
-    props.clockOuterColor !== undefined ? props.clockOuterColor : "#1C2527"
+    props.clockSettings.clockOuterColor !== undefined
+      ? props.clockSettings.clockOuterColor
+      : "#1C2527"
   );
   const [clockInnerColor] = useState(
-    props.clockInnerColor !== undefined ? props.clockInnerColor : "#FFFFFF"
+    props.clockSettings.clockInnerColor !== undefined
+      ? props.clockSettings.clockInnerColor
+      : "#FFFFFF"
   );
   const [clockOuterBorder] = useState(
-    props.clockOuterBorder !== undefined ? props.clockOuterBorder : "#999999"
+    props.clockSettings.clockOuterBorder !== undefined
+      ? props.clockSettings.clockOuterBorder
+      : "#999999"
   );
   const [timePointerColor] = useState(
-    props.timePointerColor !== undefined ? props.timePointerColor : "#CA6702"
+    props.clockSettings.timePointerColor !== undefined
+      ? props.clockSettings.timePointerColor
+      : "#CA6702"
   );
   const [faceNosColor] = useState(
-    props.faceNosColor !== undefined ? props.faceNosColor : "#EBBD3F"
+    props.clockSettings.faceNosColor !== undefined
+      ? props.clockSettings.faceNosColor
+      : "#EBBD3F"
   );
   const [digitalOn] = useState(
-    props.digitalOn !== undefined ? props.digitalOn : true
+    props.clockSettings.digitalOn !== undefined
+      ? props.clockSettings.digitalOn
+      : true
   );
   const [digitalColor] = useState(
-    props.digitalColor !== undefined ? props.digitalColor : "#CA6702"
+    props.clockSettings.digitalColor !== undefined
+      ? props.clockSettings.digitalColor
+      : "#CA6702"
   );
   const [rotationAmount, setRotationAmount] = useState(0);
   const [timeString, setTimeString] = useState("00:00:00");
   const [runClock, setRunClock] = useState("");
 
-  // buildSVG() {
-  //     let svg = document.getElementById('turningClockFace')
-  //     for (let i = 0; i < (12 * 4); i++) {
-  //         let newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-  //         newLine.setAttribute('x1', '200')
-  //         newLine.setAttribute('y1', '70')
-  //         newLine.setAttribute('x2', '200')
-  //         newLine.setAttribute('y2', '74')
-  //         newLine.style.stroke = '#999999'
-  //         newLine.style.strokeWidth = '1'
-  //         newLine.style.transform = `rotate(${i * 7.5}deg)`
-  //         newLine.style.transformOrigin = 'center'
-  //         svg.appendChild(newLine)
-  //     }
-  // }
+  const faceNos = [
+    { faceNo: "XII", x: 188 },
+    { faceNo: "I", x: 198 },
+    { faceNo: "II", x: 194 },
+    { faceNo: "III", x: 192 },
+    { faceNo: "IIII", x: 192 },
+    { faceNo: "V", x: 194 },
+    { faceNo: "VI", x: 190 },
+    { faceNo: "VII", x: 188 },
+    { faceNo: "VIII", x: 184 },
+    { faceNo: "IX", x: 192 },
+    { faceNo: "X", x: 195 },
+    { faceNo: "XI", x: 190 },
+  ];
 
-  useEffect(() => {
+  function buildTwelveHourRing() {
+    let svg = document.getElementById("turningClockFace");
+    for (let i = 0; i < 12 * 4; i++) {
+      let newLine = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "line"
+      );
+      let y1Attribute;
+      if (i % 4 === 0) {
+        y1Attribute = 69;
+      } else {
+        y1Attribute = 71;
+      }
+      newLine.setAttribute("x1", "200");
+      newLine.setAttribute("y1", y1Attribute);
+      newLine.setAttribute("x2", "200");
+      newLine.setAttribute("y2", "74");
+      newLine.setAttribute("stroke", clockOuterBorder);
+      newLine.setAttribute("transform", `rotate(${i * 7.5}, 200, 200)`);
+      svg.appendChild(newLine);
+    }
+    faceNos.forEach((_faceNo, key) => {
+      let faceNoGroup = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "g"
+      );
+      let faceNoElement = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "text"
+      );
+      faceNoElement.setAttribute("x", _faceNo.x);
+      faceNoElement.setAttribute("y", 67);
+      faceNoElement.textContent = _faceNo.faceNo;
+      faceNoElement.setAttribute("fill", faceNosColor);
+      svg.appendChild(faceNoGroup);
+      faceNoGroup.appendChild(faceNoElement);
+      faceNoGroup.setAttribute(
+        "transform",
+        `rotate(${(360 / 12) * key}, 200, 200)`
+      );
+    });
+  }
+
+  const mount = () => {
+    buildTwelveHourRing();
     getTheTime();
     setRunClock(setInterval(getTheTime, 1000));
 
     return () => {
       clearInterval(runClock);
     };
-  }, []);
+  };
+
+  useEffect(mount, []);
 
   function getTheTime() {
     let dateNow = new Date();
@@ -72,125 +133,152 @@ function FaceTurnClock(props) {
       (m < 10 ? "0" + m : m) +
       ":" +
       (s < 10 ? "0" + s : s);
-    // let timeString = '00:00:00'
 
     setRotationAmount(hoursRotationSet);
     setTimeString(timeString);
   }
 
-  const divStyle = {
-    transform: `rotate(-${rotationAmount}deg)`,
-    transformOrigin: "center",
-  };
+  const clockFaceRotation = `rotate(-${rotationAmount}, 200, 200)`;
 
   return (
     <div className={"clockContainer"}>
       <svg width={size} height={size} viewBox="0 0 400 400">
-        <circle r="152" cx="200" cy="200" fill={clockOuterColor} />
-        <circle
-          r="150"
-          cx="200"
-          cy="200"
-          fill={clockOuterColor}
-          strokeWidth={1}
-          stroke={clockOuterBorder}
-        />
-        <circle
-          r="125"
-          cx="200"
-          cy="200"
-          fill={clockOuterColor}
-          strokeWidth={1}
-          stroke={clockOuterBorder}
-        />
+        <g id="outerRing">
+          <circle r="152" cx="200" cy="200" fill={clockOuterColor} />
+          <circle
+            r="150"
+            cx="200"
+            cy="200"
+            fill={clockOuterColor}
+            strokeWidth={1}
+            stroke={clockOuterBorder}
+          />
+          <circle
+            r="125"
+            cx="200"
+            cy="200"
+            fill={clockOuterColor}
+            strokeWidth={1}
+            stroke={clockOuterBorder}
+          />
+        </g>
+
+        <g id="turningClockFace" transform={clockFaceRotation}></g>
+
         <circle r="123" cx="200" cy="200" fill={clockInnerColor} />
-        <line
-          id="minutes"
-          x1="200"
-          y1="120"
-          x2="200"
-          y2="80"
-          strokeWidth={1}
-          stroke={timePointerColor}
-        />{" "}
-        />
-        <line
-          id="minutes"
-          x1="200"
-          y1="80"
-          x2="197"
-          y2="86"
-          strokeWidth={1}
-          stroke={timePointerColor}
-        />
-        <line
-          id="minutes"
-          x1="200"
-          y1="80"
-          x2="203"
-          y2="86"
-          strokeWidth={1}
-          stroke={timePointerColor}
-        />
-        <g id="turningClockFace" style={divStyle}>
-          <text x="188" y="67" fill={faceNosColor}>
-            XII
+
+        <g id="twelveHourPointer">
+          <line
+            id="minutes"
+            x1="200"
+            y1="120"
+            x2="200"
+            y2="80"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+          <line
+            id="minutes"
+            x1="200"
+            y1="80"
+            x2="197"
+            y2="86"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+          <line
+            id="minutes"
+            x1="200"
+            y1="80"
+            x2="203"
+            y2="86"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+        </g>
+
+        <g id="daysRing">
+          <circle r="48" cx="135" cy="195" fill={clockOuterColor} />
+          <circle
+            r="46"
+            cx="135"
+            cy="195"
+            fill={clockOuterColor}
+            strokeWidth={1}
+            stroke={clockOuterBorder}
+          />
+          <circle
+            r="36"
+            cx="135"
+            cy="195"
+            fill={clockOuterColor}
+            strokeWidth={1}
+            stroke={clockOuterBorder}
+          />
+          <circle r="34" cx="135" cy="195" fill={clockInnerColor} />
+        </g>
+
+        <g id="daysPointer">
+          <line
+            x1="135"
+            y1="162"
+            x2="135"
+            y2="180"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+          <line
+            x1="135"
+            y1="162"
+            x2="133"
+            y2="166"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+          <line
+            x1="135"
+            y1="162"
+            x2="137"
+            y2="166"
+            strokeWidth={1}
+            stroke={timePointerColor}
+          />
+        </g>
+
+        <g id="turningDaysRing">
+          {/* <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+            MON
           </text>
-          <g className={"transform30deg"}>
-            <text x="198" y="67" fill={faceNosColor}>
-              I
+          <g transform="rotate(51.43 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              TUE
             </text>
           </g>
-          <g className={"transform60deg"}>
-            <text x="194" y="67" fill={faceNosColor}>
-              II
+          <g transform="rotate(102.86 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              WED
             </text>
           </g>
-          <g className={"transform90deg"}>
-            <text x="192" y="67" fill={faceNosColor}>
-              III
+          <g transform="rotate(154.29 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              THU
             </text>
           </g>
-          <g className={"transform120deg"}>
-            <text x="188" y="67" fill={faceNosColor}>
-              IIII
+          <g transform="rotate(205.71 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              FRI
             </text>
           </g>
-          <g className={"transform150deg"}>
-            <text x="194" y="67" fill={faceNosColor}>
-              V
+          <g transform="rotate(257.14 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              SAT
             </text>
           </g>
-          <g className={"transform180deg"}>
-            <text x="190" y="67" fill={faceNosColor}>
-              VI
+          <g transform="rotate(308.57 135 195)">
+            <text x="125" y="157" font-size="0.5rem" style="fill: #EBBD3F">
+              SUN
             </text>
-          </g>
-          <g className={"transform210deg"}>
-            <text x="188" y="67" fill={faceNosColor}>
-              VII
-            </text>
-          </g>
-          <g className={"transform240deg"}>
-            <text x="184" y="67" fill={faceNosColor}>
-              VIII
-            </text>
-          </g>
-          <g className={"transform270deg"}>
-            <text x="192" y="67" fill={faceNosColor}>
-              IX
-            </text>
-          </g>
-          <g className={"transform300deg"}>
-            <text x="195" y="67" fill={faceNosColor}>
-              X
-            </text>
-          </g>
-          <g className={"transform330deg"}>
-            <text x="190" y="67" fill={faceNosColor}>
-              XI
-            </text>
-          </g>
+          </g> */}
         </g>
       </svg>
       <div
@@ -200,7 +288,7 @@ function FaceTurnClock(props) {
           style={{
             color: digitalColor,
             width: Math.round(size / 12) * ((8 / 10) * 5.8),
-            fontSize: Math.round(size / 12),
+            fontSize: Math.round(size / 14),
           }}
         >
           {timeString}
