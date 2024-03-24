33128bc4824b8b213c7217de9a4bc8985f108d96
robings <58939401+robings@users.noreply.github.com>
2024-03-24 18:44:55 +0000

    Update packages, update root API.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import FaceTurnClock from "./Components/FaceTurnClock/FaceTurnClock";
 
 // const clockSettings = {
@@ -26,9 +26,8 @@ import FaceTurnClock from "./Components/FaceTurnClock/FaceTurnClock";
 
 const clockSettings = {
   size: 400,
-}
+};
 
-ReactDOM.render(
-  <FaceTurnClock clockSettings={clockSettings} />,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<FaceTurnClock clockSettings={clockSettings} />);
